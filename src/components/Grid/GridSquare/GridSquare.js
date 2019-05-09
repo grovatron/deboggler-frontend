a37938190120cbf51bef2359c05a4e0c53877d09
grovatron <grover.a.sundstrom@gmail.com>
2019-05-09 14:59:38 -0400

    Rearrange attributes

--- src/components/Grid/GridSquare/GridSquare.js
+++ src/components/Grid/GridSquare/GridSquare.js
@@ -2,11 +2,12 @@ import React from 'react';
 import styles from './GridSquare.module.css';
 
 const GridSquare = (props) => {
+
+  const value = props.value !== 0 ? props.value : null;
+  const mod = props.modifier !== 'none' ? props.modifier: null;
+
   return (
-    <div
-      className={styles['grid-square']}
-      points={props.value !== 0 ? props.value : null}
-      mod={props.modifier !== 'none' ? props.modifier : null}>
+    <div className={styles['grid-square']} points={value} mod={mod}>
       <input
         type='text'
         value={props.letter}
