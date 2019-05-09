7977c832911b36d0ba5ce962a5a50d80df178fd9
grovatron <grover.a.sundstrom@gmail.com>
2019-05-09 13:49:36 -0400

    Implement TextInput

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import styles from './TextInput.module.css';
 
 const TextInput = (props) => {
   return (
-    <div />
+    <div className={styles['text-input']}>
+      <input type='text'/>
+      <div className={styles.btn}>Submit</div>
+    </div>
   )
 }
 
