97aeaa354f5f18e45b906f4c18938146bb90748b
grovatron <grover.a.sundstrom@gmail.com>
2019-05-09 17:34:24 -0400

    Add instructions for adding modifiers

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -4,11 +4,14 @@ import styles from './TextInput.module.css';
 const TextInput = (props) => {
   return (
     <div className={styles['text-input']}>
-      <input
-        type='text'
-        value={props.value}
-        onChange={props.changeText}/>
-      <div className={styles.btn} onClick={props.handleSubmit}>Submit</div>
+      <div className={styles['input-wrapper']}>
+        <input
+          type='text'
+          value={props.value}
+          onChange={props.changeText}/>
+        <div className={styles.btn} onClick={props.handleSubmit}>Submit</div>
+      </div>
+      <p>Click the squares to add modifiers!</p>
     </div>
   )
 }
