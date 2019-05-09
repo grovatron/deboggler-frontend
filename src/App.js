af77b1284e6bfe64924ef96fbe3bce60144b4aaf
grovatron <grover.a.sundstrom@gmail.com>
2019-05-09 12:47:55 -0400

    Turn App into a stateful component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { Component }from 'react';
 import Modal from './components/Modal/Modal';
 import Header from './components/Header/Header';
 import Deboggler from './components/Deboggler/Deboggler';
 import Footer from './components/Footer/Footer';
 
-function App() {
-  return (
-    <div>
-      <Modal />
-      <Header />
-      <Deboggler />
-      <Footer />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <Modal />
+        <Header />
+        <Deboggler />
+        <Footer />
+      </div>
+    );
+  }
+
 }
 
 export default App;
