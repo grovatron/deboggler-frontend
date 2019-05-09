0f9da0538fff0e704cb078ecadb6edcfa7705ddf
grovatron <grover.a.sundstrom@gmail.com>
2019-05-09 12:46:25 -0400

    Create skeleton for app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Modal from './components/Modal/Modal';
+import Header from './components/Header/Header';
+import Deboggler from './components/Deboggler/Deboggler';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    
+    <div>
+      <Modal />
+      <Header />
+      <Deboggler />
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/components/Deboggler/Deboggler.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Deboggler = (props) => {
+  return (
+    <div />
+  )
+}
+
+export default Deboggler;
